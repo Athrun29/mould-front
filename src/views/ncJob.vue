712bc5336506e655a153a4e3f2a9694d6614b143
MaiBenBen <1728828099@qq.com>
2018-08-05 20:26:51 +0800

    vacny web

--- src/views/ncJob.vue
+++ src/views/ncJob.vue
@@ -15,61 +15,63 @@
       </label><br/>
     </div>
     <Button type="success" icon="ios-trash-outline" @click="clearModel"> 清空</Button>
-    <Button type="primary" icon="ios-hammer" @click="postData"> 确定</Button>
+    <Button type="primary" icon="ios-hammer" @click="postData" v-bind:disabled="btnDis"> 确定</Button>
 
   </div>
 </template>
 
 <script>
-  import {mapState} from 'vuex'
+	import {mapState} from 'vuex'
 
-  export default {
-    name: "ncJob",
-    data() {
-      return {
-        param: {
-          input: "",
-          output: ""
-        },
-        res: ""
-      }
-    },
-    computed: {
-      ...mapState([
-        "testUrl"
-      ])
-    },
-    methods: {
-      postData() {
-        if(this.param.input == ""){
-          this.$Message.info('输入目录不能为空');
-          return false
-        }
-        if(this.param.output == ""){
-          this.$Message.info('输出目录不能为空');
-          return false
-        }
-        this.$http.post(this.testUrl.url + "/job/ncJob",
-          this.param)
-          .then(function (res) {
-            if (res.body.success == 1) {
-              this.res = res.body.data;
-              // alert("获取成功");
-            } else {
-              this.res = "处理失败"
-            }
-          })
-      },
-      clearModel() {
-        this.param.input = "";
-        this.param.output = "";
-        this.res = "";
-      }
-    },
-    created: function () {
+	export default {
+		name: "ncJob",
+		data() {
+			return {
+				param: {
+					input: "",
+					output: ""
+				},
+				res: "",
+				btnDis: false
+			}
+		},
+		computed: {
+			...mapState([
+				"testUrl"
+			])
+		},
+		methods: {
+			postData() {
+				if (this.param.input == "") {
+					this.$Message.info('输入目录不能为空');
+					return false
+				}
+				if (this.param.output == "") {
+					this.$Message.info('输出目录不能为空');
+					return false
+				}
+				this.btnDis = true;
+				this.$http.post(this.testUrl.url + "/job/ncJob",
+					this.param)
+					.then(function (res) {
+						if (res.body.success == 1) {
+							this.res = res.body.data;
+						} else {
+							this.res = "处理失败"
+						}
+						this.btnDis = false;
+					})
+			},
+			clearModel() {
+				this.param.input = "";
+				this.param.output = "";
+				this.res = "";
+			}
+		},
+		created: function () {
 
-    }
-  }
+		}
+	}
 </script>
 
 <style>
@@ -83,11 +85,13 @@
     color: red;
     line-height: 40px;
   }
-  .search label p{
+
+  .search label p {
     display: inline-block;
     line-height: 24px;
     font-size: 14px;
   }
+
   .ivu-btn {
     font-size: 14px;
   }
