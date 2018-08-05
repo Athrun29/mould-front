5a52284773c3d39d7bffa26bfd373d3094432c98
MaiBenBen <1728828099@qq.com>
2018-08-05 21:47:26 +0800

    vacny web

--- src/views/ncJob.vue
+++ src/views/ncJob.vue
@@ -21,57 +21,60 @@
 </template>
 
 <script>
-	import {mapState} from 'vuex'
+  import {mapState} from 'vuex'
 
-	export default {
-		name: "ncJob",
-		data() {
-			return {
-				param: {
-					input: "",
-					output: ""
-				},
-				res: "",
-				btnDis: false
-			}
-		},
-		computed: {
-			...mapState([
-				"testUrl"
-			])
-		},
-		methods: {
-			postData() {
-				if (this.param.input == "") {
-					this.$Message.info('输入目录不能为空');
-					return false
-				}
-				if (this.param.output == "") {
-					this.$Message.info('输出目录不能为空');
-					return false
-				}
-				this.btnDis = true;
-				this.$http.post(this.testUrl.url + "/job/ncJob",
-					this.param)
-					.then(function (res) {
-						if (res.body.success == 1) {
-							this.res = res.body.data;
-						} else {
-							this.res = "处理失败"
-						}
-						this.btnDis = false;
-					})
-			},
-			clearModel() {
-				this.param.input = "";
-				this.param.output = "";
-				this.res = "";
-			}
-		},
-		created: function () {
+  export default {
+    name: "ncJob",
+    data() {
+      return {
+        param: {
+          input: "",
+          output: ""
+        },
+        res: "",
+        btnDis: false
+      }
+    },
+    computed: {
+      ...mapState([
+        "testUrl"
+      ])
+    },
+    methods: {
+      postData() {
+        if (this.param.input == "") {
+          this.$Message.info('输入目录不能为空');
+          return false
+        }
+        if (this.param.output == "") {
+          this.$Message.info('输出目录不能为空');
+          return false
+        }
+        this.btnDis = true;
+        this.$http.post(this.testUrl.url + "/job/ncJob",
+          this.param)
+          .then(function (res) {
+            if (res.body.success == 1) {
+              this.res = res.body.data;
+            } else {
+              this.res = "处理失败"
+            }
+            this.btnDis = false;
+          },function () {
+            this.res = "处理失败";
+            this.btnDis = false;
+          })
+      },
+      clearModel() {
+        this.param.input = "";
+        this.param.output = "";
+        this.res = "";
+      }
+    },
+    created: function () {
 
-		}
-	}
+    }
+  }
 </script>
 
 <style>
